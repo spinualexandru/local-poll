fd61b8d9b8f5d3422d25a1d11458fa0b7cbc0c25
Alex Spinu <spinualexandru@outlook.com>
2025-08-12 17:42:36 +0300

    feat: basic voting

--- src/client/public/services/vote.mjs
+++ src/client/public/services/vote.mjs
@@ -9,3 +9,19 @@ export const getVotesByPollId = (pollId) => {
     .then((result) => JSON.parse(result))
     .catch((error) => console.error(error));
 };
+
+export const castVote = (pollId, optionId) => {
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ pollId, optionId }),
+    redirect: "follow",
+  };
+
+  return fetch("/api/vote/cast", requestOptions)
+    .then((response) => response.text())
+    .then((result) => JSON.parse(result))
+    .catch((error) => console.error(error));
+};

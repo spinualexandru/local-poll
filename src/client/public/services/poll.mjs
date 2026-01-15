84198c93c8b4ddf68095ad330d1c2f53fd91476c
Spinu Alex <spinualexandru@outlook.com>
2026-01-16 00:02:58 +0200

    feat: complete flow and homepage

--- src/client/public/services/poll.mjs
+++ src/client/public/services/poll.mjs
@@ -9,3 +9,17 @@ export const getPollById = (id) => {
     .then((result) => JSON.parse(result))
     .catch((error) => console.error(error));
 };
+
+export const createPoll = (pollData) => {
+  const requestOptions = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(pollData),
+    redirect: "follow",
+  };
+
+  return fetch("/api/poll/create", requestOptions)
+    .then((response) => response.text())
+    .then((result) => JSON.parse(result))
+    .catch((error) => console.error(error));
+};
